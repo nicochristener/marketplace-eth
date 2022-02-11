2dae49dceea40a0605d38482d98c763a33e49fe2
nicochristener <diavista@gmail.com>
2022-02-11 15:34:35 +0100

    final base

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 
+import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common"
+import { CourseList } from "@components/course"
+import { OrderCard } from "@components/order"
+import { EthRates, WalletBar } from "@components/web3"
 
 export default function Home() {
   return (
-    <>
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  <div className="shrink-0">
-    <img className="h-12 w-12" src="vercel.svg" alt="ChitChat Logo"/>
-  </div>
-  <div>
-    <div clclassNameass="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-slate-500">You have a new message!</p>
-  </div>
-</div>
-    </>
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <Navbar />
+          <div className="fit">
+            <Hero />
+            <Breadcrumbs />
+            <WalletBar />
+            <EthRates />
+            <OrderCard />
+            <CourseList />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
